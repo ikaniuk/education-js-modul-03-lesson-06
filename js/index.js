529b578ab67980f95aa2eb1df79f04b6550455cb
Ihor Kaniuk <ikanyuk@icloud.com>
2023-09-24 21:28:57 +0300

    update

--- js/index.js
+++ js/index.js
@@ -72,3 +72,22 @@
 //   console.log(book.author);
 //   console.log(book.rating);
 // }
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "На берегу спокойных вод",
+//     author: "Роберт Шекли",
+//     rating: 8.51,
+//   },
+// ];
+
+// for (const book of books) {
+//   console.log(book.title);
+//   console.log(book.author);
+//   console.log(book.rating);
+// }
