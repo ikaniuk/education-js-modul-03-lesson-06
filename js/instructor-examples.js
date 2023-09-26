5c2a0cc6cd6548b4184fa519f346e36a7e908096
Ihor Kaniuk <ikanyuk@icloud.com>
2023-09-26 22:25:33 +0300

    complete-instructor-2

---
+++ js/instructor-examples.js
@@ -0,0 +1,98 @@
+// Модуль 3 Занятие 6. Деструктуризация и rest/spread
+// Example 1 - Деструктуризация
+// Перепиши функцию так, чтобы она принимала один объект параметров,
+// вместо набора независимых аргументов.
+
+// function calcBMI(weight, height) {
+//   const numericWeight = Number(weight.replace(',', '.'));
+//   const numericHeight = Number(height.replace(',', '.'));
+//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
+// }
+
+// // Было
+// console.log(calcBMI('88,3', '1.75'));
+// console.log(calcBMI('68,3', '1.65'));
+// console.log(calcBMI('118,3', '1.95'));
+// console.log(calcBMI('1.75', '88,3'));
+
+// // Ожидается
+// console.log(
+//   calcBMI({
+//     weight: '88,3',
+//     height: '1.75',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '68,3',
+//     height: '1.65',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '118,3',
+//     height: '1.95',
+//   }),
+// );
+
+// =============================================================================
+// const calcBMI = function ({ weight, height }) {
+//     const numericWeight = Number(weight.replace(',', '.'));
+//     const numericHeight = Number(height.replace(',', '.'));
+//     return Number((numericWeight / numericHeight ** 2).toFixed(1));
+// };
+
+
+// console.log(
+//   calcBMI({
+//     weight: '88,3',
+//     height: '1.75',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '68,3',
+//     height: '1.65',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '118,3',
+//     height: '1.95',
+//   }),
+// );
+
+
+// Example 2 - Деструктуризация
+// Перепиши функцию так, чтобы она принимала один объект параметров,
+// вместо набора независимых аргументов.
+
+// function printContactsInfo(names, phones) {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// Было
+// printContactsInfo(
+//   'Jacob,William,Solomon,Artemis',
+//   '89001234567,89001112233,890055566377,890055566300',
+// );
+
+
+// =====================================================================================
+
+// const printContactsInfo = function ({phones, names}) {
+//     const nameList = names.split(',');
+//     const phonesList = phones.split(',');
+//     for (let i = 0; i < nameList.length; i += 1){
+//         console.log(`${nameList[i]}: ${phonesList[i]}`)
+//     }
+// }
+// // Ожидается
+// printContactsInfo({
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// });
